596a7e35377a4a430435769c6169b36a60b7d018
developer-sazzad <developersazzad@gmail.com>
2024-09-13 17:30:46 +0600

    Completed Bus Ticket Fonted Design with HTML, Css, Tailwind, DaisyUI

---
+++ scripts/select-seet.js
@@ -0,0 +1,9 @@
+
+
+console.log('Select Seet JS File');
+document.getElementById('a1').addEventListener('click', function(){
+    const getLeftSeet = document.getElementById('left-seet').innerHTML;
+    const getLeftSeetString = getLeftSeet.innerHTML;
+    
+    console.log(getLeftSeet);
+})
---
+++ scripts/utility.js
@@ -0,0 +1,34 @@
+
+// function checkTicket(){
+//     const element = document.getElementById('f4');
+//     if(element.cl){
+//         element.body.style.backgroundColor = 'blue';
+//     }
+// }
+
+// document.getElementById('f4').addEventListener('click', function(){
+//     document.body.style.backgroundColor = 'green';
+// })
+
+
+
+// function changeColor(){
+//     const button = document.getElementsByClassName('.bg-ticket');
+//     if(button.style.backgroundColor === '#fff'){
+//         button.style.backgroundColor = '#000';
+//     }
+//     else{
+//         button.style.backgroundColor = '#fff';
+//     }
+// }
+
+// function seetClick(){
+//     seetLeft('left-seet');
+// }
+console.log('Utility Files')
+document.getElementById('a1').addEventListener('click', function(){
+    const getLeftSeet = document.getElementById('left-seet').innerHTML;
+    // const getLeftSeetString = getLeftSeet.innerHTML;
+    
+    console.log(getLeftSeet);
+})
